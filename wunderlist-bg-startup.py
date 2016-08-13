16dd24f5063f44aa66449ae173f9f877d49c2695
Sameera Shaakunthala <sameera.shaakunthala@pearson.com>
2016-08-14 00:22:35 +0530

    Fixed issue #2

--- wunderlist-bg-startup.py
+++ wunderlist-bg-startup.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 
-import time, os
+import time, os, sys
 
+a = sys.argv
 wait_time = 900
+
+try:
+  a[1]
+except IndexError:
+  pass
+else:
+  try:
+    wait_time = int(a[1]) * 60
+  except ValueError:
+    wait_time = 900
+  else:
+    wait_time = int(a[1]) * 60
 
 while True:
   print ("WRAPPER: STARTING ITERATION")
